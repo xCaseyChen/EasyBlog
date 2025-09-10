ca20106d83ec2fe0220c3b3c1f73d9dd7ef029fe
CaseyChen <xcaseychen@protonmail.com>
2025-09-10 22:22:00 +0800

    feat: add before_id and limit query for /api/posts?

--- server/handlers/guest/apis.go
+++ server/handlers/guest/apis.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/lib/pq"
@@ -17,51 +18,89 @@ import (
 )
 
 func postsQueryHandler(db *gorm.DB) httprouter.Handle {
+	limitMax := 100
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		w.Header().Set("Content-Type", "application/json") // response type: json
 		// json response
 		type jsonResponse struct {
-			Success    bool                 `json:"success"`
-			Message    string               `json:"message"`
-			PostBriefs []database.PostBrief `json:"post_briefs"`
+			Success      bool                 `json:"success"`
+			Message      string               `json:"message"`
+			PostBriefs   []database.PostBrief `json:"post_briefs"`
+			NextBeforeID *uint                `json:"next_before_id"`
 		}
 		// parse params list
 		tags := utils.ParseQueryList(r, "tags", ",")
 		category := utils.ParseQueryList(r, "category", ",")
-		// check category
-		if len(category) > 1 {
+		beforeId := utils.ParseQueryList(r, "before_id", ",")
+		limit := utils.ParseQueryList(r, "limit", ",")
+		// check category, limit, before_id len
+		if len(category) > 1 || len(limit) > 1 || len(beforeId) > 1 {
 			w.WriteHeader(http.StatusBadRequest)
 			_ = json.NewEncoder(w).Encode(jsonResponse{
-				Success:    false,
-				Message:    "too many categories",
-				PostBriefs: nil,
+				Success:      false,
+				Message:      "too many category/limit/before_id",
+				PostBriefs:   nil,
+				NextBeforeID: nil,
 			})
 			return
 		}
-		// use tags and category search in database
-		var post_briefs []database.PostBrief
-		var query gorm.ChainInterface[database.PostBrief] = gorm.G[database.PostBrief](db)
+		// use tags, category, before_id, limit search in database
+		query := gorm.G[database.PostBrief](db).Order("id desc")
+		if len(limit) == 1 {
+			limitInt, err := strconv.Atoi(limit[0])
+			if err != nil || limitInt <= 0 || limitInt >= limitMax {
+				w.WriteHeader(http.StatusBadRequest)
+				_ = json.NewEncoder(w).Encode(jsonResponse{
+					Success:      false,
+					Message:      "invalid limit: " + limit[0],
+					PostBriefs:   nil,
+					NextBeforeID: nil,
+				})
+				return
+			}
+			query = query.Limit(limitInt)
+		}
+		if len(beforeId) == 1 {
+			beforeIdInt, err := strconv.Atoi(beforeId[0])
+			if err != nil || beforeIdInt <= 0 {
+				w.WriteHeader(http.StatusBadRequest)
+				_ = json.NewEncoder(w).Encode(jsonResponse{
+					Success:      false,
+					Message:      "invalid before_id: " + beforeId[0],
+					PostBriefs:   nil,
+					NextBeforeID: nil,
+				})
+				return
+			}
+			query = query.Where("id < ?", beforeIdInt)
+		}
 		if len(tags) > 0 {
 			query = query.Where("tags @> ?", pq.Array(tags))
 		}
 		if len(category) == 1 {
 			query = query.Where("category = ?", category[0])
 		}
-		post_briefs, err := query.Find(r.Context())
+		postBriefs, err := query.Find(r.Context())
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_ = json.NewEncoder(w).Encode(jsonResponse{
-				Success:    false,
-				Message:    "internal server error",
-				PostBriefs: nil,
+				Success:      false,
+				Message:      "internal server error",
+				PostBriefs:   nil,
+				NextBeforeID: nil,
 			})
 			return
 		}
+		var nextBeforeID *uint = nil
+		if len(postBriefs) != 0 {
+			nextBeforeID = &postBriefs[len(postBriefs)-1].ID
+		}
 		w.WriteHeader(http.StatusOK)
 		_ = json.NewEncoder(w).Encode(jsonResponse{
-			Success:    true,
-			Message:    "find post briefs",
-			PostBriefs: post_briefs,
+			Success:      true,
+			Message:      "find post briefs",
+			PostBriefs:   postBriefs,
+			NextBeforeID: nextBeforeID,
 		})
 	}
 }
