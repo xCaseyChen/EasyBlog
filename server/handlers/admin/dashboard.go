94653fee05e78cea44c01033dfc87531162907f8
CaseyChen <xcaseychen@protonmail.com>
2025-09-19 14:08:17 +0800

    feat: add admin dashboard page

--- server/handlers/admin/dashboard.go
+++ server/handlers/admin/dashboard.go
@@ -1,15 +1,26 @@
 package admin
 
 import (
+	"easyblog/handlers/common"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 	"gorm.io/gorm"
 )
 
 func dashboardHandler(db *gorm.DB) httprouter.Handle {
+	const dashboardTemplateName = "dashboard"
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprint(w, "Admin Dashboard page\n")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		var htmlString strings.Builder
+		if err := tmpl[dashboardTemplateName].Execute(&htmlString, nil); err != nil {
+			common.RenderInfoPage(w, http.StatusInternalServerError, "internal server error")
+			log.Printf("Failed to execute template %s: %v", dashboardTemplateName, err)
+			return
+		}
+		fmt.Fprint(w, htmlString.String())
 	}
 }
---
+++ server/handlers/admin/templates.go
@@ -0,0 +1,23 @@
+package admin
+
+import (
+	"html/template"
+	"log"
+)
+
+var adminTemplates = map[string][]string{
+	"dashboard": {"templates/layout_admin/index.html", "templates/dashboard/index.html"},
+}
+
+var tmpl map[string]*template.Template
+
+func init() {
+	tmpl = make(map[string]*template.Template)
+	for name, paths := range adminTemplates {
+		t, err := template.ParseFiles(paths...)
+		if err != nil {
+			log.Fatalf("Failed to parse template %v: %v", paths, err)
+		}
+		tmpl[name] = t
+	}
+}
