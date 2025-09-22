69235673282ec469b4083f0fa3c41a02b6d7385d
CaseyChen <xcaseychen@protonmail.com>
2025-09-22 19:38:25 +0800

    feat: add api routes

--- server/handlers/admin/routes.go
+++ server/handlers/admin/routes.go
@@ -10,12 +10,28 @@ import (
 )
 
 var adminGetHandlers = map[string]func(*gorm.DB) httprouter.Handle{
-	"/admin/dashboard": dashboardHandler, // admin dashboard page
-	"/api/admin/ping":  pingHandler,      // admin ping api
+	"/admin/dashboard":         dashboardHandler,  // admin dashboard page
+	"/admin/posts/:id/preview": previewHandler,    // admin preview post page
+	"/api/admin/ping":          pingHandler,       // admin ping api
+	"/api/admin/posts/:id":     postsInfoHandler,  // admin post brief and detail api
+	"/api/admin/posts":         postsQueryHandler, // admin post list query api
 }
 
 var adminPostHandlers = map[string]func(*gorm.DB) httprouter.Handle{
-	"/api/admin/logout": logoutHandler, // admin logout api
+	"/api/admin/logout": logoutHandler,    // admin logout api
+	"/api/admin/posts":  postsPostHandler, // admin posts POST (new post with title)
+}
+
+var adminDeleteHandlers = map[string]func(*gorm.DB) httprouter.Handle{
+	"/api/admin/posts/:id": postsDeleteHandler, // admin posts DELETE (delete posts)
+}
+
+var adminPatchHandlers = map[string]func(*gorm.DB) httprouter.Handle{
+	"/api/admin/posts/:id": postsPatchHandler, // admin posts PATCH (modify status)
+}
+
+var adminPutHandlers = map[string]func(*gorm.DB) httprouter.Handle{
+	"/api/admin/posts/:id": postsPutHandler, // admin posts PUT (update posts)
 }
 
 func withAuth(h httprouter.Handle, jwtSecret string) httprouter.Handle {
@@ -35,5 +51,14 @@ func Routes(r *httprouter.Router, db *gorm.DB, jwtSecret string) {
 	}
 	for path, handler := range adminPostHandlers {
 		r.POST(path, withAuth(handler(db), jwtSecret))
+	}
+	for path, handler := range adminDeleteHandlers {
+		r.DELETE(path, withAuth(handler(db), jwtSecret))
+	}
+	for path, handler := range adminPatchHandlers {
+		r.PATCH(path, withAuth(handler(db), jwtSecret))
+	}
+	for path, handler := range adminPutHandlers {
+		r.PUT(path, withAuth(handler(db), jwtSecret))
 	}
 }
